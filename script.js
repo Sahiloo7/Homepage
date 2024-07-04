6812a795599cc814e7b8097d971f578c8c1963a2
Sahiloo7 <red_oo7@icloud.com>
2024-07-04 13:43:53 +0530

    adding html files for more pages

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+// Home section
+document.querySelector('.home').addEventListener('click', function() {
+    window.location.href = 'index.html';
+});
+
 // About section
 document.querySelector('.about').addEventListener('click', function() {
     window.location.href = 'about.html';
+});
+
+// Experience section
+document.querySelector('.experience').addEventListener('click', function() {
+    window.location.href = 'exp.html';
+});
+
+// Contact section
+document.querySelector('.contact').addEventListener('click', function() {
+    window.location.href = 'cont.html';
 });
